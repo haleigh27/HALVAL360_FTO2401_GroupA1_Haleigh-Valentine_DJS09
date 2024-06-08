5ba6dd2b2cee27b14fb35404b5156cccdeba7b0d
Haleigh <haleighvalentine@gmail.com>
2024-06-08 21:11:49 +0200

    Strings Types mini-challenge

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 console.log('Script loaded');
-// Number Types mini-challenge 10 10.2
-// Write a function that will only accept numbers and attend to
-// all TypeScript weakness flags.
-// : number
+
+// String Types mini-challenge
+// Write a function that will display the most recent reviewers name next to the review total,
+// making sure to assign a type to the parameter, to prevent unwanted behaviour.
+// : string
+
 const reviewTotalDisplay = document.querySelector('#reviews') as HTMLElement;
 
 const reviews = [
@@ -27,8 +29,8 @@ const reviews = [
 ];
 
 // Solution
-function showReviewTotal(value: number) {
-	reviewTotalDisplay.innerHTML = 'review total ' + value.toString();
+function showReviewTotal(value: number, reviewer: string) {
+	reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer;
 }
 
-showReviewTotal(reviews.length);
+showReviewTotal(reviews.length, reviews[0].name);
